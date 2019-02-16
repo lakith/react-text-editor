049a7948a9301f74577485242602191afc5f7799
lakith <lakith1995@gmail.com>
2019-02-16 20:52:56 +0530

    wrap up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'jodit';
+import 'jodit/build/jodit.min.css';
+import JoditEditor from "jodit-react";
 
 class App extends Component {
+
+  state = {
+    content: 'content',
+  }
+
+  updateContent = (value) => {
+    this.setState({content:value})
+  }
+
+  jodit;
+	setRef = jodit => this.jodit = jodit;
+	
+	config = {
+		readonly: false // all options from https://xdsoft.net/jodit/doc/
+  }
+  
+
   render() {
+    console.log(this.state.content)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{width:500, marginLeft:50,marginTop:50}}>
+        <JoditEditor
+            	editorRef={this.setRef}
+                value={this.state.content}
+                config={this.config}
+                onChange={this.updateContent}
+          />
       </div>
     );
   }
